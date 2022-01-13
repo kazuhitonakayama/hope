93fb6cd2878116a4cbb197556347740fc13174a9
kazuhitonakayama <firstjin19990101@gmail.com>
2022-01-13 11:17:48 +0900

    json形式の感染者数のデータをGoの構造体に変換する

--- main.go
+++ main.go
@@ -8,28 +8,37 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	// "github.com/line/line-bot-sdk-go/linebot"
+	// "github.com/line/line-bot-sdk-go/linebot"Cqq
 )
 
-type Book struct {
-	Id int `json:"id"`
-	Title string `json:"title"`
-	Contents string `json:"contents"`
+type Infector struct {
+	ItemList []ItemList `json:"itemList"`
+}
+
+type ItemList struct {
+    Date      string `json:"date"`
+    NameJp    string `json:"name_jp"`
+    Npatients string `json:"npatients"`
 }
 
 func main() {
-	resp, err := http.Get("https://gcal8havkk.execute-api.ap-northeast-1.amazonaws.com/Prod/books")
+	// resp, err := http.Get("https://xcodaxq124.execute-api.ap-northeast-1.amazonaws.com/Prod/books")
+    resp, err := http.Get("https://opendata.corona.go.jp/api/Covid19JapanAll")
     // エラーに値があればログに出力し終了する
     if err != nil {
         log.Fatal(err)
     }
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
-	var books []Book
-	if err := json.Unmarshal(body, &books); err != nil {
+
+	var infectors Infector
+	if err := json.Unmarshal(body, &infectors); err != nil {
         log.Fatal(err)
     }
-	fmt.Println(string(body))
+    
+    for _, infector := range infectors.ItemList {
+        fmt.Printf("%s \n", infector)
+    }
 
     // // LINE Botクライアント生成する
     // // BOT にはチャネルシークレットとチャネルトークンを環境変数から読み込み引数に渡す
