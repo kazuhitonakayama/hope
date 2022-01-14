8a679030ecff06807e3ea848cdb82b15f6273f8d
kazuhitonakayama <firstjin19990101@gmail.com>
2022-01-14 21:55:52 +0900

    最新の感染者数を取得し、全国の情報を一覧で表示する

--- go.mod
+++ go.mod
@@ -4,3 +4,5 @@ module hope
 go 1.17
 
 require github.com/line/line-bot-sdk-go v7.8.0+incompatible
+
+require github.com/go-sql-driver/mysql v1.6.0 // indirect
--- main.go
+++ main.go
@@ -2,28 +2,61 @@ package main
 
 import (
 	"log"
-	// "strings"
-	// "os"
+	// // "strings"
+	"os"
 	"encoding/json"
 	"fmt"
+	// "database/sql"
+	// "fmt"
 	"io"
+	// "log"
+	// "encoding/json"
+	// _ "github.com/go-sql-driver/mysql"
 	"net/http"
-	// "github.com/line/line-bot-sdk-go/linebot"Cqq
+	// "net/url"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
-
-type Infector struct {
-	ItemList []ItemList `json:"itemList"`
-}
 
 type ItemList struct {
     Date      string `json:"date"`
     NameJp    string `json:"name_jp"`
-    Npatients string `json:"npatients"`
+    Npatients int `json:"npatients"`
+    Diff int `json:"diff"`
+	CreatedAt string `json:"created_at"`
 }
 
 func main() {
-	// resp, err := http.Get("https://xcodaxq124.execute-api.ap-northeast-1.amazonaws.com/Prod/books")
-    resp, err := http.Get("https://opendata.corona.go.jp/api/Covid19JapanAll")
+    itemlists := FetchLatestInfectors()
+    
+    header := "最新の感染者数速報です🎉 \n感染者数が多い順で並べてます👍 \n\n"
+    text := header
+    for _ , item := range itemlists {
+        text = text + fmt.Sprintf("%s：%d（前日比 + %d 人）\n", item.NameJp, item.Npatients, item.Diff)
+    }
+
+    // LINE Botクライアント生成する
+    // BOT にはチャネルシークレットとチャネルトークンを環境変数から読み込み引数に渡す
+    bot, err := linebot.New(
+        os.Getenv("LINE_BOT_CHANNEL_SECRET"),
+        os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
+    )
+	// エラーに値があればログに出力し終了する
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // テキストメッセージを生成する
+    message := linebot.NewTextMessage(text)
+    // テキストメッセージを友達登録しているユーザー全員に配信する
+    if _, err := bot.BroadcastMessage(message).Do(); err != nil {
+        log.Fatal(err)
+    }
+}
+
+func FetchLatestInfectors() []ItemList{
+    api_url := "https://s0oe7xjx7i.execute-api.ap-northeast-1.amazonaws.com/Prod/infectors"
+
+    resp, err := http.Get(api_url)
     // エラーに値があればログに出力し終了する
     if err != nil {
         log.Fatal(err)
@@ -31,30 +64,10 @@ func main() {
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 
-	var infectors Infector
-	if err := json.Unmarshal(body, &infectors); err != nil {
+	var itemlist []ItemList
+	if err := json.Unmarshal(body, &itemlist); err != nil {
         log.Fatal(err)
     }
-    
-    for _, infector := range infectors.ItemList {
-        fmt.Printf("%s \n", infector)
-    }
 
-    // // LINE Botクライアント生成する
-    // // BOT にはチャネルシークレットとチャネルトークンを環境変数から読み込み引数に渡す
-    // bot, err := linebot.New(
-    //     os.Getenv("LINE_BOT_CHANNEL_SECRET"),
-    //     os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
-    // )
-	// // エラーに値があればログに出力し終了する
-    // if err != nil {
-    //     log.Fatal(err)
-    // }
-
-    // // テキストメッセージを生成する
-    // message := linebot.NewTextMessage("hello, world")
-    // // テキストメッセージを友達登録しているユーザー全員に配信する
-    // if _, err := bot.BroadcastMessage(message).Do(); err != nil {
-    //     log.Fatal(err)
-    // }
+    return itemlist
 }
